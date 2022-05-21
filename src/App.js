35ef2ae5d70f9d06040fe1d7850a51da3c9514db
Smiledpk <98518871+Smiledpk@users.noreply.github.com>
2022-05-21 21:03:00 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { BiArchive } from "react-icons/bi"
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl "><BiArchive />  Your Appointment </h1>
+    </div>
+  );
+}
+
+export default App;
